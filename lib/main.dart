645c923699383bcbc8e3b2f8e5f964336a1d1ef6
marti <marticrest@gmail.com>
2025-01-30 12:43:46 +0100

    registyration_form

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
  
+import 'package:flutter/material.dart';
+import 'screens/registration_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Registration Form",
+      home: RegistrationScreen(),
+    );
+  }
+}
